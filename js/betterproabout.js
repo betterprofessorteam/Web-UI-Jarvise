5a9b1d202fd23ff5c148ab62fedbc22c5c094543
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-27 17:15:02 -0500

    new javascript file.

--- js/betterproabout.js
+++ js/betterproabout.js
@@ -1,14 +1,3 @@
-const signupbtn = document.querySelector("#btn1");
-signupbtn.addEventListener('mouseleave', event =>{
-  event.target.style.background = 'black';
-});
-
-const signupbtn2 = document.querySelector("#btn1");
-signupbtn2.addEventListener('mouseenter', event =>{
-  event.target.style.background = 'dodgerblue';
-});
-
-
 const upperButton1 = document.querySelector("#up_btn")
 upperButton1.addEventListener('mouseleave', event =>{
   event.target.style.background = 'dodgerblue';

1c6d5006c233c81eb1626e46037fed7158766396
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-27 13:01:08 -0500

    Index,html addition plus more less additions.

--- js/betterpro.js
+++ js/betterpro.js
@@ -1,9 +1,9 @@
-var signupbtn = document.querySelector('btn_signup');
-signupbtn.addEventListener('click', function(event){
-  event.target.style.color = 'red';
+var signupbtn = document.querySelector("btn_getStarted");
+signupbtn.addEventListener('mouseleave', function(event){
+  event.target.style.color = 'none';
 });
 
-var signupbtn = document.querySelector('btn_signup');
-signupbtn.addEventListener('mouseenter', function(event){
-  event.target.style.border = '2px solid black';
+var signupbtn2 = document.querySelector("btn_getStarted");
+signupbtn2.addEventListener('mouseenter', function(event){
+  event.target.style.color = '2px solid red';
 });
